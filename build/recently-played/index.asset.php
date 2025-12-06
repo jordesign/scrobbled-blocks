49d166e4ccfba56268bf9292814c3493f32c66ef
“Jordan <jordan@jordesign.com>
2025-12-07 09:08:28 +1100

    First build, and ignore Node modules.

---
+++ build/recently-played/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ea7b0f75bc90f4b917d8');
